1fb23d97d3ef1e940fba69986aed1438f98dcbd1
Felipe Ramos <132492020+RanndaLL0@users.noreply.github.com>
2025-04-29 13:07:19 -0300

    criado o titulo para os livros

--- learnify_front_end/src/screens/book_page/index.tsx
+++ learnify_front_end/src/screens/book_page/index.tsx
@@ -1,4 +1,6 @@
-import { Container,ScrollableView,BookCover,ImageArea,InfoContainer } from "./styles"
+import { Container,ScrollableView,BookCover,ImageArea,
+        InfoContainer, BookTitle,AuthorTitle,Description
+        ,DescriptionContainer,PurchaseArea,PriceTag,OldPrice } from "./styles"
 import { ImageBackground } from "react-native"
 
 export default function BookPage() {
@@ -16,9 +18,30 @@ export default function BookPage() {
                     </BookCover>
                 </ImageArea>
                 <InfoContainer>
-                        
+                    <BookTitle>
+                        Near to the Wild Heart
+                    </BookTitle>
+                    <AuthorTitle>
+                        Clarice Lispector
+                    </AuthorTitle>
+                    <DescriptionContainer>
+                        <Description>
+                            Near to the Wild Heart, published in Rio de Janeiro in 1943, 
+                            introduced Brazil to what one writer called "Hurricane Clarice" 
+                            a twenty-three-year-old girl who wrote her first book in a tiny rented 
+                            room and then baptized it with a title taken from Joyce: "He was alone, 
+                            unheeded, near to the wild heart of life."
+                            The book was an unprecedented sensation -- the discovery of a genius. 
+                            Narrative epiphanies and interior monologue frame the life of Joana, 
+                            from her middle-class childhood through her unhappy marriage and its dissolution 
+                            to transcendence, when she proclaims: "I shall arise as strong and comely as a young colt."
+                        </Description>
+                    </DescriptionContainer>
                 </InfoContainer>
             </ScrollableView>
+            <PurchaseArea>
+                <PriceTag>$16.99 <OldPrice>$20.00</OldPrice></PriceTag>
+            </PurchaseArea>
         </Container>
     )
 }
--- learnify_front_end/src/screens/book_page/styles.tsx
+++ learnify_front_end/src/screens/book_page/styles.tsx
@@ -1,35 +1,77 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-    flex: 1;
-    align-items: center;
+  flex: 1;
+  align-items: center;
 `;
-    
-export const ScrollableView = styled.ScrollView`
-    width: 100%;
-    padding: 0 5%;
-    background-color: #121212;
-    flex: 1;
-`
+
+export const ScrollableView = styled.View`
+  width: 100%;
+  padding: 0 5%;
+  background-color: #121212;
+  flex: 1;
+`;
 
 export const BookCover = styled.Pressable`
-    width: 300;
-    height: 400;
-    border-radius: 10px;
-`
+  width: 245px;
+  height: 345px;
+  border-radius: 10px;
+`;
 
 export const ImageArea = styled.View`
-    width: 100%;
-    flex: 1;
-    align-items: center;
-    justify-content: center;
-    margin-top: 40px;
-`
+  width: 100%;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`;
 
 export const InfoContainer = styled.View`
-    width: 90%;
-    background-color: white;
-    height: 40;
-    margin: 0 auto;
-    margin-top: 20;
+  width: 100%;
+  height: auto;
+  margin: 0 auto;
+  margin-top: 20px;
+`;
+
+export const BookTitle = styled.Text`
+  font-size: 28px;
+  font-weight: 600;
+  color: white;
+  margin-bottom: 2px;
+`;
+
+export const AuthorTitle = styled.Text`
+  font-size: 18px;
+  color: gray;
+  font-weight: 500;
+`;
+
+export const DescriptionContainer = styled.ScrollView`
+  width: 100%;
+  height: 140px;
+  margin-top: 20px;
+`;
+
+export const Description = styled.Text`
+  font-size: 16px;
+  color: gray;
+`;
+
+export const PurchaseArea = styled.View`
+  background-color: #121212;
+  flex: 0.20;
+  width: 100%;
+  padding: 20px 20px;
+`;
+
+export const PriceTag = styled.Text`
+  color: white;
+  font-weight: 400;
+  font-size: 26;
+`;
+
+export const OldPrice = styled.Text `
+  font-size: 18;
+  color: gray;
+  text-decoration: line-through;
 `
